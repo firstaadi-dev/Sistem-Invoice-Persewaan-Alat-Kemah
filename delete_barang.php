8d07543e0b1933f1ef236f5c44ba01919346549a
Firsta Adi Pradana <firstaadi@hotmail.com>
2020-12-02 16:40:38 +0700

    Fix comment

--- delete_barang.php
+++ delete_barang.php
@@ -2,12 +2,12 @@
 // include database connection file
 include_once("config.php");
 
-// Get id from URL to delete that user
+
 $id = $_GET['id'];
 
-// Delete user row from table based on given id
+
 $result = mysqli_query($mysqli, "DELETE FROM barang WHERE id=$id");
 
-// After delete redirect to Home, so that latest user list will be displayed.
+
 header("Location:daftar_barang.php");
 ?>
--- delete_member.php
+++ delete_member.php
@@ -2,12 +2,12 @@
 // include database connection file
 include_once("config.php");
 
-// Get id from URL to delete that user
+// Get id from URL to delete 
 $id = $_GET['id'];
 
-// Delete user row from table based on given id
+
 $result = mysqli_query($mysqli, "DELETE FROM member WHERE id=$id");
 
-// After delete redirect to Home, so that latest user list will be displayed.
+
 header("Location:daftar_member.php");
 ?>
